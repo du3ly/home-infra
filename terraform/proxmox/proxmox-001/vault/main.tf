768b040318068533eb8840798bc076767655255a
Duely Yung <dyung2@gmail.com>
2023-02-03 19:48:19 -0600

    remove comments

--- terraform/proxmox/proxmox-001/vault/main.tf
+++ terraform/proxmox/proxmox-001/vault/main.tf
@@ -1,7 +1,4 @@
 terraform {
-  # Partial configuration
-  # Backend conn_str configured with env variable 
-  # export TF_CLI_ARGS="-backend-config='conn_str=postgres://user:pass@host/terraform'"
   backend "pg" {}
 }
 
